84bd3cb24208972d3341c09a6ff6c14e9c4ed294
Cyril Gibaud <cyril.gibaud@toonkit-studio.com>
2014-08-21 14:24:06 +0200

    Milestone Generation/publishing - Can Generate "Preston" Asset flawlessly - Can process a simple "publishing" step (still need to publish parameters)

--- Dialogs/TKMessageBox.cs
+++ Dialogs/TKMessageBox.cs
@@ -5,8 +5,44 @@ using System.Windows.Forms;
 
 namespace TK.GraphComponents.Dialogs
 {
+    public enum InputTypes
+    {
+        String, Float, Double, Int, Bool
+    }
+
+    public class RichDialogResult
+    {
+        DialogResult _result = DialogResult.None;
+        object _data = null;
+
+        public RichDialogResult(DialogResult inResult, object inData)
+        {
+            _result = inResult;
+            _data = inData;
+        }
+
+        public DialogResult Result
+        {
+            get { return _result; }
+            set { _result = value; }
+        }
+
+        public object Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+    }
+
     public class TKMessageBox
     {
+        public static RichDialogResult ShowInput(InputTypes inType, string Message, string Caption)
+        {
+            TextInputForm form = new TextInputForm(Message, Caption, "");
+            DialogResult result = form.ShowDialog();
+            return new RichDialogResult(result, form.InputValue);
+        }
+
         public static DialogResult ShowMessage(string Message, string Caption)
         {
             LongMessageForm form = new LongMessageForm();
